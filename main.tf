7eeb3dd1bdc57bfda1410a220b64f18742684450
Nick Pan <panick@panick-macbookpro3.roam.corp.google.com>
2020-04-09 18:12:26 -0400

    Add 3 subnetwork, 1 vm resource in main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,53 @@ provider "google" {
 
   project = "network-playground-273716"
   region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_subnetwork" "us-central1-subnetwork-dmz" {
+    name =          "dmz-us-central1-terraform"
+    ip_cidr_range = "10.10.0.0/20"
+    region =        "us-central1"
+    network =       google_compute_network.vpc_network.name
+}
+
+resource "google_compute_subnetwork" "us-central1-subnetwork-int" {
+    name =          "int-us-central1-terraform"
+    ip_cidr_range = "10.8.0.0/20"
+    region =        "us-central1"
+    network =       google_compute_network.vpc_network.name
+}
+
+resource "google_compute_subnetwork" "us-central1-subnetwork-mgmt" {
+    name =          "mgmt-us-central1-terraform"
+    ip_cidr_range = "10.6.0.0/26"
+    region =        "us-central1"
+    network =       google_compute_network.vpc_network.name
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "production-network-dmz-int-mgmt"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.us-central1-subnetwork-dmz.name
+    access_config {
+        nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
 }
